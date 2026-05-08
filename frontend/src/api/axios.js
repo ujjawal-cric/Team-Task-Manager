b7af5ea8210e77bfb6a0986d65213a60fa3d5eba
ujjawal-cric <ujjawal.root@gmail.com>
2026-05-09 05:02:10 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://team-task-manager-production-300d.up.railway.app/api",
+  baseURL:
+    "https://team-task-manager-production-300d.up.railway.app/api",
 });
 
 API.interceptors.request.use((req) => {
-  const user = JSON.parse(localStorage.getItem("user"));
+  const user = JSON.parse(
+    localStorage.getItem("user")
+  );
 
   if (user?.token) {
     req.headers.Authorization = `Bearer ${user.token}`;
