4686f1f20efbbd67bc7aabecd825a236b0d325ec
ujjawal-cric <ujjawal.root@gmail.com>
2026-05-09 04:52:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
