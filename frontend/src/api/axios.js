904ed47c440ba3ba97314fa415cf257dbe57451c
ujjawal-cric <ujjawal.root@gmail.com>
2026-05-09 04:57:42 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "team-task-manager-production-300d.up.railway.app/api",
+  baseURL: "https://team-task-manager-production-300d.up.railway.app/api",
 });
 
 API.interceptors.request.use((req) => {
