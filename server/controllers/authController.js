d69f7d03f3992ee608bf6461edd750442851f0d2
ujjawal-cric <ujjawal.root@gmail.com>
2026-05-09 21:48:30 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -65,20 +65,21 @@ const registerUser = asyncHandler(
 
     let userRole = "member";
 
-    // ADMIN ACCESS ONLY WITH SECRET
     if (role === "admin") {
-  if (
-    adminSecret !==
-    process.env.ADMIN_SECRET
-  ) {
-    return res.status(401).json({
-      message:
-        "Invalid admin secret key",
-    });
-  }
+      if (
+        adminSecret !==
+        process.env.ADMIN_SECRET
+      ) {
+        res.status(401);
 
-  finalRole = "admin";
-}
+        throw new Error(
+          "Invalid admin secret key"
+        );
+      }
+
+      finalRole = "admin";
+    }
+
     const user =
       await User.create({
         name,
