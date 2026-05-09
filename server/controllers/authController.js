cd12baad99b06d9653d6d689068a8ff820a2dc00
ujjawal-cric <ujjawal.root@gmail.com>
2026-05-09 22:02:10 +0530

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,4 @@
-const asyncHandler = require(
+/*const asyncHandler = require(
   "express-async-handler"
 );
 
@@ -65,19 +65,13 @@ const registerUser = asyncHandler(
 
     let userRole = "member";
 
-    if (role === "admin") {
-      if (
-        adminSecret !==
+    // ADMIN ACCESS ONLY WITH SECRET
+    if (
+      role === "admin" &&
+      adminSecret ===
         process.env.ADMIN_SECRET
-      ) {
-        res.status(401);
-
-        throw new Error(
-          "Invalid admin secret key"
-        );
-      }
-
-      finalRole = "admin";
+    ) {
+      userRole = "admin";
     }
 
     const user =
@@ -86,6 +80,155 @@ const registerUser = asyncHandler(
         email,
         password,
         role: userRole,
+      });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      token: generateToken(
+        user._id
+      ),
+    });
+  }
+);
+
+const loginUser = asyncHandler(
+  async (req, res) => {
+    const { email, password } =
+      req.body;
+
+    const user =
+      await User.findOne({
+        email,
+      });
+
+    if (
+      user &&
+      (await user.matchPassword(
+        password
+      ))
+    ) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        token: generateToken(
+          user._id
+        ),
+      });
+    } else {
+      res.status(401);
+
+      throw new Error(
+        "Invalid credentials"
+      );
+    }
+  }
+);
+
+const getMe = asyncHandler(
+  async (req, res) => {
+    res.json(req.user);
+  }
+);
+
+module.exports = {
+  registerUser,
+  loginUser,
+  getMe,
+};*/
+const asyncHandler = require(
+  "express-async-handler"
+);
+
+const validator = require("validator");
+
+const User = require("../models/User");
+
+const generateToken = require(
+  "../utils/generateToken"
+);
+
+const registerUser = asyncHandler(
+  async (req, res) => {
+    const {
+      name,
+      email,
+      password,
+      role,
+      adminSecret,
+    } = req.body;
+
+    if (
+      !name ||
+      !email ||
+      !password
+    ) {
+      res.status(400);
+
+      throw new Error(
+        "Please fill all fields"
+      );
+    }
+
+    if (
+      !validator.isEmail(email)
+    ) {
+      res.status(400);
+
+      throw new Error(
+        "Invalid email format"
+      );
+    }
+
+    if (password.length < 6) {
+      res.status(400);
+
+      throw new Error(
+        "Password must be at least 6 characters"
+      );
+    }
+
+    const userExists =
+      await User.findOne({
+        email,
+      });
+
+    if (userExists) {
+      res.status(400);
+
+      throw new Error(
+        "User already exists"
+      );
+    }
+
+    // DEFAULT ROLE
+    let finalRole = "member";
+
+    // ADMIN CHECK
+    if (role === "admin") {
+  if (
+    adminSecret !==
+    process.env.ADMIN_SECRET
+  ) {
+    return res.status(401).json({
+      message:
+        "Invalid admin secret key",
+    });
+  }
+
+  finalRole = "admin";
+}
+
+    const user =
+      await User.create({
+        name,
+        email,
+        password,
+        role: finalRole,
       });
 
     res.status(201).json({
